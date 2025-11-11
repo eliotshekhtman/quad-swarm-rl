175524a3751bf74c1a9342486eca840a9e281139
eliotshekhtman <eliotshekhtman@gmail.com>
2025-11-10 23:15:47 -0500

    Reduced time before swapping goal

--- gym_art/quadrotor_multi/scenarios/patrol_dual_goal.py
+++ gym_art/quadrotor_multi/scenarios/patrol_dual_goal.py
@@ -17,7 +17,7 @@ class Scenario_patrol_dual_goal(QuadrotorScenario):
         self.steps_since_switch = None
         self.switch_radius = self.approch_goal_metric
         # Small dwell window prevents rapid toggling when hovering on the boundary
-        self.min_switch_interval = int(0.25 * self.envs[0].control_freq)
+        self.min_switch_interval = int(0.2 * self.envs[0].control_freq)
 
     def _get_point(self):
         box_size = self.envs[0].box
@@ -32,14 +32,19 @@ class Scenario_patrol_dual_goal(QuadrotorScenario):
     def _generate_patrol_pairs(self):
         """Random endpoints for patrol pairs."""
         goal_pairs = np.zeros((self.num_agents, 2, 3), dtype=np.float64)
-        for i in range(self.num_agents):
-            goal_pairs[i, 0] = self._get_point() 
-            end_point = self._get_point()
-            # Ensure end point isn't too close to starting point
-            while np.linalg.norm(goal_pairs[i, 0] - end_point) <= self.switch_radius * 5:
+        if self.num_agents == 1: # Special case: go back and forth around (0,0)
+            goal_pairs[0, 0] = np.array([2, 2, 1])
+            goal_pairs[0, 1] = np.array([-2, -2, 1])
+            self.goal_pairs = goal_pairs
+        else: 
+            for i in range(self.num_agents):
+                goal_pairs[i, 0] = self._get_point() 
                 end_point = self._get_point()
-            goal_pairs[i, 1] = end_point
-        self.goal_pairs = goal_pairs
+                # Ensure end point isn't too close to starting point
+                while np.linalg.norm(goal_pairs[i, 0] - end_point) <= self.switch_radius * 5:
+                    end_point = self._get_point()
+                goal_pairs[i, 1] = end_point
+            self.goal_pairs = goal_pairs
 
     def _activate_goals(self):
         self.active_goal_index = np.zeros(self.num_agents, dtype=np.int64)
